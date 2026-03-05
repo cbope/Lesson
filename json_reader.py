bb7eb747c8093347ded6d87605b5875f0aa0f755
norbeck <crbope@gmail.com>
2026-03-04 19:58:50 -0500

    added sort function to json reader.

--- json_reader.py
+++ json_reader.py
@@ -1,9 +1,16 @@
 import json
 
-with open("example.json") as jsonfile:
-    print(jsonfile)
-    line = json.loads(jsonfile)
-    for row in line:
-        print(' '.join(row))
+# with open("example.json") as jsonfile:
+#     print(jsonfile)
+#     line = json.loads(jsonfile)
+#     for row in line:
+#         print(' '.join(row))
 
 # couldn't get this to work.
+
+
+list1 = ["egg", "bread", "milk"]
+
+list1.sort()
+
+print(list1)
